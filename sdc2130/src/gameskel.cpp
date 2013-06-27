b69877ac7b009c16b1f3cb1e1a315081965b8372
SKEL Lab <skel@RoboSKEL.(none)>
2013-06-27 13:10:04 +0200

    mode select

--- sdc2130/src/gameskel.cpp
+++ sdc2130/src/gameskel.cpp
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <vector>
-
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <AL/al.h>
+#include <AL/alut.h>
+*/
 #include "sdc2130_skel/RoboteqDevice.h"
 #include "sdc2130_skel/ErrorCodes.h"
 #include "sdc2130_skel/Constants.h"
@@ -16,12 +22,15 @@ float functions[14]={0};
 int LM = 0 ;
 int RM = 0 ;
 int HB = 0 ; //handbrake signal
-int ESD = 0; //emergency shutdown signal
+int ESD = 0 ; //emergency shutdown signal
+int MODE = 0 ;
 double gp_funcs[14] = {0};
 string response = "";
 RoboteqDevice device;
 int status = -1;
 int i ;
+
+//ALuint sndBuffer, sndSource;
 
 void teleopCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 {	
@@ -32,14 +41,15 @@ void teleopCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 		functions[i]=msg->data[i];
 		i++;
 	}
-	
-	////////////////////////////
-	//PRESS START FOR SHUTDOWN 
-	/////////////////////////////
-    if(functions[11]==1)
-    {
-		printf("Initializing Emergency Shutdown Protocol pewpepwewpew\n");
-		if((status = device.SetCommand(_GO, 2,  0) != RQ_SUCCESS))
+	//////////////////////////////////////////////////////
+	//PRESS SELECT TO ALTERNATE BETWEEN NAVIGATION METHODS
+	//////////////////////////////////////////////////////
+	if(functions[10]==1)
+	{
+		if (MODE==0)
+		{
+			MODE=1;
+			if((status = device.SetCommand(_GO, 2,  0) != RQ_SUCCESS))
 			{
 				cout<<"failed --> \n"<<status<<endl;
 				device.Disconnect();
@@ -62,55 +72,63 @@ void teleopCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 			{
 				//cout<<"succeeded.\n"<<endl;
 			}
-		printf("Dave, stop. Stop, will you? Stop, Dave. Will you stop Dave? Stop, Dave.\n");
-		ros::Duration(3).sleep();
-		ESD = 1 ;
-		ros::shutdown();
-		return ;
-	}
-	////////////////////////////////////////////
-	//PRESS O BUTTON FOR 5 SECOND IMMOBILIZATION
-	///////////////////////////////////////////
-	else if (functions[3]==1)
-	{	
-		//ROS_INFO("Temporary Immobilization Protocol Engaged. Stand By.\n");
-		printf("Temporary Immobilization Protocol Engaged. Stand By.\n");
-		//printf("first IF\n");
-		if((status = device.SetCommand(_GO, 2,  0) != RQ_SUCCESS))
-		{
-			cout<<"failed --> \n"<<status<<endl;
-			device.Disconnect();
-			ros::shutdown();
-			
-		}	
-		else 
-		{
-			//cout<<"succeeded.\n"<<endl;
 		}
-		ros::Duration(0.01).sleep(); 
-		//printf("Second IF\n");
-		if((status = device.SetCommand(_GO, 1,- 0)) != RQ_SUCCESS)
-		{
-			cout<<"failed --> \n"<<status<<endl;
-			device.Disconnect();
-			ros::shutdown();
-		}	
 		else
 		{
-			//cout<<"succeeded.\n"<<endl;
+			MODE=0;
 		}
-		ros::Duration(5).sleep();
 	}
-	////////////////////////////////////////////////////
-	//PRESS TRIANGLE FOR HANDBRAKE PRESS AGAIN TO RESUME
-	////////////////////////////////////////////////////
-	else if (functions[2]==1)
+	if(MODE==0)
 	{
-		if(HB==0)
+		////////////////////////////
+		//PRESS START FOR SHUTDOWN 
+		/////////////////////////////
+		if(functions[11]==1)
 		{
-			HB=1;
-			//ROS_INFO("Handbrake Engaged. Press Again to Resume Normal Function\n");
-			printf("Handbrake Engaged. Press Again to Resume Normal Function\n");
+			printf("Initializing Emergency Shutdown Protocol pewpepwewpew\n");
+			if((status = device.SetCommand(_GO, 2,  0) != RQ_SUCCESS))
+				{
+					cout<<"failed --> \n"<<status<<endl;
+					device.Disconnect();
+					ros::shutdown();
+					
+				}	
+			else 
+				{
+					//cout<<"succeeded.\n"<<endl;
+				}
+				ros::Duration(0.01).sleep(); 
+				//printf("Second IF\n");
+			if((status = device.SetCommand(_GO, 1,- 0)) != RQ_SUCCESS)
+				{
+					cout<<"failed --> \n"<<status<<endl;
+					device.Disconnect();
+					ros::shutdown();
+				}	
+			else
+				{
+					//cout<<"succeeded.\n"<<endl;
+				}
+			printf("Dave, stop. Stop, will you? Stop, Dave. Will you stop Dave? Stop, Dave.\n\n");
+			ros::Duration(3).sleep();
+			ESD = 1 ;
+			ros::shutdown();
+			return ;
+		}
+		////////////////////////////////////////////
+		//PRESS O BUTTON FOR 5 SECOND IMMOBILIZATION
+		///////////////////////////////////////////
+		else if (functions[3]==1)
+		{	
+				/*
+				alSourcei (sndSource, AL_BUFFER, sndBuffer);
+				alSourcePlay (sndSource);
+				alutSleep (1);
+				alutExit ();
+				*/
+			
+			//ROS_INFO("Temporary Immobilization Protocol Engaged. Stand By.\n");
+			printf("Temporary Immobilization Protocol Engaged. Stand By.\n\n");
 			//printf("first IF\n");
 			if((status = device.SetCommand(_GO, 2,  0) != RQ_SUCCESS))
 			{
@@ -135,18 +153,56 @@ void teleopCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 			{
 				//cout<<"succeeded.\n"<<endl;
 			}
+			ros::Duration(5).sleep();
+			printf("Motion Resumed\n\n");
 		}
+		////////////////////////////////////////////////////
+		//PRESS TRIANGLE FOR HANDBRAKE PRESS AGAIN TO RESUME
+		////////////////////////////////////////////////////
+		else if (functions[2]==1)
+		{
+			if(HB==0)
+			{
+				HB=1;
+				ROS_INFO("Handbrake Engaged. Press Again to Resume Normal Function\n");
+				printf("Handbrake Engaged. Press Again to Resume Normal Function\n");
+				//printf("first IF\n");
+				if((status = device.SetCommand(_GO, 2,  0) != RQ_SUCCESS))
+				{
+					cout<<"failed --> \n"<<status<<endl;
+					device.Disconnect();
+					ros::shutdown();
+					
+				}	
+				else 
+				{
+					//cout<<"succeeded.\n"<<endl;
+				}
+				ros::Duration(0.01).sleep(); 
+				//printf("Second IF\n");
+				if((status = device.SetCommand(_GO, 1,- 0)) != RQ_SUCCESS)
+				{
+					cout<<"failed --> \n"<<status<<endl;
+					device.Disconnect();
+					ros::shutdown();
+				}	
+				else
+				{
+					//cout<<"succeeded.\n"<<endl;
+				}
+			}
+			else
+			{
+				HB=0;
+				printf("Handbrake Disengaged. Motion Resumed. \n\n");
+			}
+			ros::Duration(1).sleep();
+		}
+		//////////////
+		//MOVE MOTORS
+		/////////////
 		else
 		{
-			HB=0;
-		}
-		ros::Duration(2).sleep();
-	}
-	//////////////
-	//MOVE MOTORS
-	/////////////
-	else
-	{
 		if(HB==0)
 		{
 			if (functions[0] >= 0)
@@ -187,7 +243,8 @@ void teleopCallback(const std_msgs::Float64MultiArray::ConstPtr& msg)
 			}
 			//ros::Duration(0.01).sleep();
 		}
-	}     
+	}
+}     
 }
 
 int main(int argc, char *argv[])
@@ -222,9 +279,17 @@ int main(int argc, char *argv[])
     else
       cout<<"returned --> "<<result<<endl;
     ROS_INFO("Roboteq -- Successfull setup of the Roboteq SDC2130");
-    printf ("Sek Operational\n");
+    printf ("Sek Operational\n\n");
     ros::Duration(0.01).sleep(); //sleep for 10 ms
-
+    
+    
+			/*
+			alutInit (&argc, argv);
+			sndBuffer = alutCreateBufferFromFile("/home/skel/r2ack.wav");
+			alGenSources (1, &sndSource);
+			*/
+    
+    
 while (ros::ok())
     {	
 		if (ESD==1)
